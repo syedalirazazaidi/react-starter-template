646eb04fea1b7ddfa153a2ee49f7f34bdc622c69
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-03-02 02:03:11 +0500

    add react-router-dom

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable prettier/prettier */
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
-import { WrappedApp, App } from './App';
+import App from './App';
 
 describe('App', () => {
   it('Renders hello world', () => {
